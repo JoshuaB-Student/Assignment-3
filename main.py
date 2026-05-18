06faaf98bfc6ae37b2258e825102cefd7415764f
JoshuaB-Student <S403595@students.cdu.edu.au>
2026-05-18 19:52:30 +0930

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-from image_processor import ImageProcessor
-import cv2
+from game import SpotTheDifferenceGame
 
-
-processor = ImageProcessor()
-
-image = processor.load_image(
-    "sample.jpg"
-)
-
-modified = processor.generate_differences(
-    image
-)
-
-cv2.imshow(
-    "Original Image",
-    image
-)
-
-cv2.imshow(
-    "Modified Image",
-    modified
-)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+game = SpotTheDifferenceGame()
+game.run()
